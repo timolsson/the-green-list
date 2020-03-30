e37df64b5a9360a5ea96aefb2249506b9bd9936f
Tim Olsson <tim.olsson@hyperisland.se>
2020-03-30 11:10:15 +0200

    Storyblok bridge
    Added a bridge to Storyblok (-storyblok-service.js file, to be added later)

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Head from "../components/head";
 import Nav from "../components/nav";
+import StoryblokService from "../utils/storyblok-service";
 
 export default ({ children, settings = {} }) => (
   <div>
     <Head />
     <Nav settings={settings} />
-    <div className="util__container">{children}</div>
+    <div className="util__container">
+      {children}
+      {StoryblokService.bridge()}
+    </div>
 
     <style jsx global>{`
       article,
